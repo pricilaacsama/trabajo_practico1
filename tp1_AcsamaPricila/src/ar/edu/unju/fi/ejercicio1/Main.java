ed035efd766113af3b7054375a07bca8be968e60
prici <prici@prici>
2024-04-05 18:07:39 -0300

    Se agrega paquete y clase para el ejercicio 1

---
+++ tp1_AcsamaPricila/src/ar/edu/unju/fi/ejercicio1/Main.java
@@ -0,0 +1,25 @@
+package ar.edu.unju.fi.ejercicio1;
+
+public class Main {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		/** 
+		primero declarar y asignar los variables;
+		luego calcular promedio;
+		finalmente mostrar el resultado; 
+		 **/
+		
+		int Num1 = 8;
+		int Num2 = 5;
+		int Num3 = 1;
+		int Num4 = 20;
+		int Num5 = 9;
+		
+		double promedio = (Num1 + Num2 + Num3 + Num4 + Num5)/5.0;
+		
+		System.out.println("el promedio es: " + promedio);
+		
+	}
+
+}
