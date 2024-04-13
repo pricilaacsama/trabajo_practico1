d8758d08bf49d10c97b89130764b3c88412eda92
prici <prici@prici>
2024-04-12 22:15:25 -0300

    subo el ejercicio 11 para la asistencia de practica

---
+++ tp1_AcsamaPricila/src/ar/edu/unju/fi/ejercicio11/Main.java
@@ -0,0 +1,19 @@
+package ar.edu.unju.fi.ejercicio11;
+
+public class Main {
+
+	public static void main(String[] args) {
+		
+		int i = 2;
+		int j = 40;
+		
+		for (i=2; j>0; i++) {
+			System.out.print((j * i)+ " ");
+			
+			j=j-1;
+			
+		}
+		
+	}
+
+}
